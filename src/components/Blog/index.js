aad7ff0f546fdf8b34c3bfff051460fbc6a30351
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-23 01:38:08 +0300

    components: Home & Navbar components

---
+++ src/components/Blog/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../NavBar';
+const Blog = () => {
+    return (
+      <div>
+        <NavBar />
+        hi , im the blog
+      </div>
+    );
+}
+
+export default Blog
